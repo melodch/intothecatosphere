6da59a81af9c36d43da993a37c3fd19221e9f2f1
melodch <67964052+melodch@users.noreply.github.com>
2020-12-02 18:58:14 -0500

    Create ladder.py

---
+++ ladder.py
@@ -0,0 +1,30 @@
+import pygame as pg
+from OnBoard import OnBoard
+from Constants import *
+
+
+class Ladder(OnBoard):
+    """
+    Defining all the ladders in the game.
+    """
+    def __init__(self, raw_image, position):
+        """
+        Initialize the image, position and rect instance attributes
+
+        Args:
+            raw_image: A string representing the path to a png.
+            position: A tuple representing the coordinates of
+            the ladder.
+        """
+        # Using super to call the attributes from the parent OnBoard class.
+        pass
+
+    def update_image(self, raw_image):
+        """
+        Update the ladder image.
+
+        Args:
+            raw_image: A string representing the path to a png.
+        """
+        # Update the ladder image from the raw image and scale it
+        pass
