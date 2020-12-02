04d19aab9cdc6876a74a8d30f7d98236edfa423c
melodch <67964052+melodch@users.noreply.github.com>
2020-12-02 18:58:01 -0500

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,18 @@
+"""
+Set various constants that will stay the same throughout the game.
+"""
+
+# Set dimensions for all objects used in the game, such as
+# sprites, gems, ladders, etc.
+
+# Make variables with paths to png files for objects and
+# game backgrounds.
+# background: png image of game background
+# startbackground: png image of start menu background
+
+# Set fps, font, and colors.
+# fps:
+# font:
+
+# Set various colors like white as (255, 255, 255) and black as (0, 0, 0).
+# Colors use HTML5 format.
