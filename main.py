8840df5ddf2e6c72e96aff8154a56fa80cf3f4c0
melodch <67964052+melodch@users.noreply.github.com>
2020-12-02 18:57:33 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame as pg
+from Game import Game
+
+
+# Main loop for playing the game
+if __name__ == "__main__":
+    pg.init()
+    # Instantiate the Game class and run the game
+    createdGame = Game()
+    createdGame.runGame()
